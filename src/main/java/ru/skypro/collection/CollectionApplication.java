78c72f5f20259d9ea460a638d60c54615c0e9dc1
Александр <ignatenko.ap@yandex.ru>
2023-11-07 17:59:40 +0300

    collection

---
+++ src/main/java/ru/skypro/collection/CollectionApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionApplication.class, args);
+	}
+
+}
